3f55817b1de691745ce1029c92f7e9e1307863d2
Damir <125348732+ki55el@users.noreply.github.com>
2024-03-27 17:20:00 +0600

    Add lab6/thread

---
+++ SpaceBattle.Lib/SendCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Concurrent;
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class SendCommand : ICommand
+{
+    private readonly int _id;
+    private readonly ICommand _cmd;
+    public SendCommand(int id, ICommand cmd)
+    {
+        _id = id;
+        _cmd = cmd;
+    }
+    public void Execute()
+    {
+        IoC.Resolve<ConcurrentDictionary<int, BlockingCollection<ICommand>>>("Queue List")[_id].Add(_cmd);
+    }
+}
