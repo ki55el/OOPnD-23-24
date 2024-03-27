d89b23b8b24d1d9e6678a3e6e488e78098e285db
Damir <125348732+ki55el@users.noreply.github.com>
2024-03-27 17:20:00 +0600

    Add lab6/thread

---
+++ SpaceBattle.Lib/CreateAndStartThreadCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class CreateAndStartThreadCommand : ICommand
+{
+    private readonly int _id;
+    public CreateAndStartThreadCommand(int id)
+    {
+        _id = id;
+    }
+    public void Execute()
+    {
+        var queue = new BlockingCollection<ICommand>(10);
+        IoC.Resolve<ConcurrentDictionary<int, BlockingCollection<ICommand>>>("Queue List").TryAdd(_id, queue);
+
+        var st = new ServerThread(queue);
+        IoC.Resolve<ConcurrentDictionary<int, ServerThread>>("ServerThread List").TryAdd(_id, st);
+
+        st.Start();
+    }
+}
---
+++ SpaceBattle.Lib/ServerThread.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Concurrent;
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class ServerThread
+{
+    private readonly BlockingCollection<ICommand> _queue;
+    private Action _behavior;
+    private readonly Thread _thread;
+    private bool _stop = false;
+    public ServerThread(BlockingCollection<ICommand> queue)
+    {
+        _queue = queue;
+        _behavior = () =>
+        {
+            var cmd = _queue.Take();
+            try
+            {
+                cmd.Execute();
+            }
+            catch (Exception ex)
+            {
+                IoC.Resolve<ICommand>("Exception.Handler", ex, cmd).Execute();
+            }
+        };
+        _thread = new Thread(() =>
+            {
+                while (!_stop)
+                {
+                    _behavior();
+                }
+            }
+        );
+    }
+
+    public void Start()
+    {
+        _thread.Start();
+    }
+
+    internal void Stop()
+    {
+        _stop = true;
+    }
+
+    internal void SetBehavior(Action behavior)
+    {
+        _behavior = behavior;
+    }
+
+    internal BlockingCollection<ICommand> GetQueue()
+    {
+        return _queue;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return _thread.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return _thread.GetHashCode();
+    }
+}
